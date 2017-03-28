8d60ecb14b4fce1307f27b95fd06127085076546
Felipe <info@icologic.co>
2017-03-28 15:11:11 +0200

    Initial Commit

---
+++ Teoria_CoreData_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Teoria_CoreData_iOS
+//
+//  Created by Andres on 28/3/17.
+//  Copyright © 2017 icologic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
